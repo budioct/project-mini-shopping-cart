970ef5704f021d76cd25cbc24801d5c7f48063e0
Budhi Octaviansyah <budioct@gmail.com>
2023-12-19 11:06:10 +0700

    start project

---
+++ src/main/java/pt/sofco/graha/ProjectMiniBudhiOctaviansyahApplication.java
@@ -0,0 +1,13 @@
+package pt.sofco.graha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMiniBudhiOctaviansyahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMiniBudhiOctaviansyahApplication.class, args);
+	}
+
+}
